037e8850128da0bc35642c485f35bb64ac1f04ca
MacsCX <maciek-cx@protonmail.com>
2017-12-28 14:24:26 +0100

    Complete algorithm (modified CC + base64)

--- src/main/java/utils/EncryptorTest.java
+++ src/main/java/utils/EncryptorTest.java
@@ -10,8 +10,8 @@ package utils;
 public class EncryptorTest {
     public static void main(String[] args)  {
         Encryptor encryptor = new Encryptor("Kurła dobra rypka!");
-        String encrypted = encryptor.encryptString("Grażyna polej no musztardkie!");
-        String decrypted = encryptor.decryptString(encrypted);
+        String encrypted = encryptor.mixedEncryption("Grażyna polej no musztardkie!");
+        String decrypted = encryptor.mixedDecryption(encrypted);
 
         System.out.printf("Encrypted string: %s\n", encrypted);
         System.out.printf("Decrypted string: %s", decrypted);
